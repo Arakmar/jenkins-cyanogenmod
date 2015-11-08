63bddbb9c247e99b80edd0a95e84097701cf920b
Yoann Laissus <yoann.laissus@gmail.com>
2015-11-08 15:32:42 +0100

    Go back to hudson as a dir name.

--- job.sh
+++ job.sh
@@ -11,7 +11,7 @@ export WORKSPACE=$PWD
 
 if [ ! -d hudson ]
 then
-  git clone git://github.com/Arakmar/jenkins-cyanogenmod.git
+  git clone git://github.com/Arakmar/jenkins-cyanogenmod.git hudson
 fi
 
 cd hudson
